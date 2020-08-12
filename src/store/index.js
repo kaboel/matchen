4dc055bef41050a6d3c79a6a63735ad4e0947c79
Faiq <faiq.kaboel@gmail.com>
2020-08-12 18:21:02 +0700

    register player module

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import playerModule from "./modules/player/player-module";
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
   },
+  getters: {
+
+  },
   mutations: {
   },
   actions: {
   },
   modules: {
+    playerModule
   }
 })
 
