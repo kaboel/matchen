22ec65e03fd8a33b750e78eeda6c110173f80b0b
Faiq <faiq.kaboel@gmail.com>
2020-08-12 19:02:39 +0700

    remove buefy and add bulma instead

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
-// Assets
-import Buefy from "buefy"
-import 'buefy/dist/buefy.css'
+// Assets Imports
+import 'bulma/css/bulma.min.css'
 import './assets/css/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSmileWink } from '@fortawesome/free-regular-svg-icons'
-
-Vue.use(Buefy)
 
 library.add( faSmileWink )
 
