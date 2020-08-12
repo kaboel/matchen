5d31ab2a29cbc9577928e1b9c53fe0af9b1d6b36
Faiq <faiq.kaboel@gmail.com>
2020-08-12 23:52:33 +0700

    add route guard for main page

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from './store'
 
 Vue.use(VueRouter)
 
@@ -9,9 +10,23 @@ const router = new VueRouter({
     {
       path: '/',
       name: 'Dashboard',
-      component: () => import('./components/Dashboard'),
+      component: () => import('./views/Dashboard'),
       meta: {
         title: 'Matchen! A Matching Game'
+      }
+    },
+    {
+      path: '/main',
+      name: 'Main',
+      component: () => import('./views/Main'),
+      beforeEnter: async (to, from, next) => {
+        try {
+          let player = await store.getters.getPlayerName
+          if (player) await next()
+          else await next('/')
+        } catch (e) {
+          await next('/')
+        }
       }
     }
   ]
