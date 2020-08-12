b4a864604e1c44ec4ac8ba01d524e22dab262587
Faiq <faiq.kaboel@gmail.com>
2020-08-12 19:02:12 +0700

    setup store mutations for player

--- src/store/modules/player/player-module.js
+++ src/store/modules/player/player-module.js
@@ -1,4 +1,5 @@
 import Vuex from 'vuex'
+import { v4 as uuid } from 'uuid'
 import { SET_PLAYER } from './player-mutation-types'
 
 let playerModule = {
@@ -8,16 +9,34 @@ let playerModule = {
   getters: {
     getPlayer: (state) => {
       return state.currentPlayer
+    },
+    getPlayerId: (state) => {
+      let playerId = null
+      if (state.currentPlayer.hasOwnProperty('name')) {
+        playerId = state.currentPlayer.id
+      }
+      return playerId
+    },
+    getPlayerName: (state) => {
+      let playerName = null
+      if (state.currentPlayer.hasOwnProperty('name')) {
+        playerName = state.currentPlayer.name
+      }
+      return playerName
     }
   },
   mutations: {
     SET_PLAYER: (state, data) => {
-
+      let newPlayer = {
+        id: uuid(),
+        name: data
+      }
+      state.currentPlayer = newPlayer
     }
   },
   actions: {
     setPlayer: (context, data) => {
-
+      context.commit('SET_PLAYER', data)
     }
   }
 }
